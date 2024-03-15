91c0a3ce2345da4e0567e597cb850de065fb6759
MartinIlg12 <martinilguan12221@gmail.com>
2024-03-15 00:13:53 -0500

    OkeyFormularios

--- js/carrito.js
+++ js/carrito.js
@@ -9,6 +9,7 @@ let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
 const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
 const contenedorTotal = document.querySelector("#total");
 const botonComprar = document.querySelector("#carrito-acciones-comprar");
+const validarCompra = document.querySelector("#carrito-acciones-validar");
 
 
 function cargarProductosCarrito() {
@@ -129,14 +130,95 @@ function actualizarTotal() {
 }
 
 botonComprar.addEventListener("click", comprarCarrito);
-function comprarCarrito() {
 
+function comprarCarrito() {
     productosEnCarrito.length = 0;
     localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
-    
+
     contenedorCarritoVacio.classList.add("disabled");
     contenedorCarritoProductos.classList.add("disabled");
     contenedorCarritoAcciones.classList.add("disabled");
     contenedorCarritoComprado.classList.remove("disabled");
 
+    // Mensaje de compra exitosa con imagen
+    Swal.fire({
+        icon: 'success',
+        title: '¡Compra Exitosa!',
+        html: '<p>Gracias por tu compra.</p><br><hr><br><p>Se está procesando tu envio.</p><i class="bi bi-send"></i>'
+    });
 }
+
+
+
+
+validarCompra.addEventListener("click", validarCarrito);
+
+function validarCarrito() {
+    Swal.fire({
+        title: 'Ingresa los detalles de tu tarjeta de crédito',
+        html:
+            `<form id="formularioTarjeta">
+                <div class="swal2-input-container">
+                    <input type="text" id="numeroTarjeta" class="swal2-input" placeholder="Número de Tarjeta" required>
+                </div>
+                <div class="swal2-input-container">
+                    <input type="text" id="nombreTitular" class="swal2-input" placeholder="Nombre del Titular" required pattern="[a-zA-ZáéíóúÁÉÍÓÚ\s]+" title="Por favor ingresa solo letras">
+                </div>
+                <div class="swal2-input-container">
+                    <label for="fechaExpiracion" class="swal2-label" style="margin-top: 5px;">Fecha de Expiración:</label>
+                    <input type="month" id="fechaExpiracion" class="swal2-input" required>
+                </div>
+                <div class="swal2-input-container">
+                    <input type="text" id="cvv" class="swal2-input" placeholder="CVV" required pattern="[0-9]{3}" title="El CVV debe tener 3 dígitos">
+                </div>
+            </form>`,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        confirmButtonStyle: 'background-color: #4CAF50; color: white;',
+        cancelButtonText: 'Cancelar',
+        preConfirm: () => {
+            const numeroTarjeta = document.getElementById('numeroTarjeta').value;
+            const nombreTitular = document.getElementById('nombreTitular').value;
+            const fechaExpiracion = document.getElementById('fechaExpiracion').value;
+            const cvv = document.getElementById('cvv').value;
+
+            
+            const regexNumeroTarjeta = /^\d{16}$/;
+
+            
+            const regexCVV = /^\d{5}$/;
+
+            if (!numeroTarjeta.match(regexNumeroTarjeta)) {
+                Swal.showValidationMessage('Por favor ingresa un número de tarjeta válido (16 dígitos numéricos)');
+                return;
+            }
+
+            if (!nombreTitular) {
+                Swal.showValidationMessage('Por favor ingresa el nombre del titular');
+                return;
+            }
+
+            if (!cvv.match(regexCVV)) {
+                Swal.showValidationMessage('Por favor ingresa un CVV válido (5 dígitos numéricos)');
+                return;
+            }
+
+            return { numeroTarjeta, nombreTitular, fechaExpiracion, cvv };
+        }
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const detallesTarjeta = result.value;
+            Swal.fire({
+                title: '¡Ahora puedes realizar la compra!',
+                text: `Los detalles de tu tarjeta son: 
+                Número de Tarjeta: ${detallesTarjeta.numeroTarjeta}
+                Nombre del Titular: ${detallesTarjeta.nombreTitular}
+                Fecha de Expiración: ${detallesTarjeta.fechaExpiracion}
+                CVV: ${detallesTarjeta.cvv}`,
+                icon: 'success'
+            });
+        }
+    });
+}
+
